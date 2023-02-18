a7d15aec57d5f6eb710973f88290a157cb64e7e2
Parshva <36695274+shahparshva72@users.noreply.github.com>
2023-02-18 08:25:24 -0600

    Initial Commit

---
+++ CricBar/CricBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricBarApp.swift
+//  CricBar
+//
+//  Created by Parshva Shah on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct CricBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
